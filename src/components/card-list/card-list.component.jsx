060f53a50f9487062d2d244c0e359dd2191c2f32
Ebuka <peterdbrainy5@gmail.com>
2020-12-17 14:29:47 -0800

    separated out concerns in to components, card and card-list components, displayed image and emails of the monsters

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './card-list.styles.css';
+import { Card } from './card/card.component';
 
-export const CardList = props => {
+export const CardList = props => (
 // console.log(props);
-return <div className="card-list">{props.children }</div>;
+<div className="card-list"> {
+    props.monsters.map(monster => (
+       <Card key={monster.id} monster={monster}/>)
+    )}
+  </div>
 
-};
+);
