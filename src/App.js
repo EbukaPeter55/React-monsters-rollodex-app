b2d4222ff0588c9e9ad0fa50522383e743c7d458
Ebuka <peterdbrainy5@gmail.com>
2020-12-23 14:57:28 -0800

    created a reusable search component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
-
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
-     monsters: []
-    };
+     monsters: [],
+     searchField: ''
+    }; 
   }
 
   componentDidMount(){
@@ -18,9 +19,18 @@ class App extends Component {
   }
 
   render () {
+    // object destructing the state
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
     return (
           <div className="App">
-            <CardList monsters={this.state.monsters}>
+           <SearchBox
+           placeholder="search monsters" 
+           handleChange={e => this.setState({searchField: e.target.value})}
+           />
+            <CardList monsters={filteredMonsters}>
             </CardList>
        
           </div>
