62945a482820761efbd14f310bac5ab3a40bdee7
Ebuka <peterdbrainy5@gmail.com>
2021-01-05 23:34:56 +0100

    reduced the sizes of the monsters

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
     this.state = {
      monsters: [],
      searchField: ''
--- src/components/card-list/card/card.component.jsx
+++ src/components/card-list/card/card.component.jsx
@@ -6,7 +6,7 @@ export const Card = props => (
     <div className="card-container">
     <img 
     alt="monster" 
-    src={`https://robohash.org/${props.monster.id}?set=set2`} 
+    src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
     />
     <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
